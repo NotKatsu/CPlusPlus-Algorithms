d1d0e7d5807620cb7c57234576900d429610797d
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-21 12:09:24 +0100

    Change variable name

--- Couting Sort/functions/search.cpp
+++ Couting Sort/functions/search.cpp
@@ -1,10 +1,12 @@
 int counting_sort(int number_to_find, int array[]) {
-    int found_item = 0; 
+    int found_item_count = 0; 
     int size_of_array = sizeof(array) / sizeof(array[0])
     
     for (int count = 0; count == size_of_array; count++) {
         if (array[count] == number_to_find) {
-            found_item++;
+            found_item_count++;
         }
     }
+
+    return found_item_count;
 }
